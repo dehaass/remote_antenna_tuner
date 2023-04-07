2a61aa8e5149e1f3d8d774b8ea9a5fe5ecb305a1
Stuart de Haas <stuartdehaas@gmail.com>
2023-04-06 20:33:28 -0700

    Working step transmit

--- src/Radio_float_pass.ino
+++ src/Radio_float_pass.ino
@@ -45,7 +45,10 @@ void setup() {
 
   Serial.begin(115200);
 
-  digitalWrite(enable_pin, LOW); //Active low
+  digitalWrite(enable_pin, HIGH); //Active low
+  pinMode(enable_pin, OUTPUT);
+  pinMode(step_pin, OUTPUT);
+  pinMode(direction_pin, OUTPUT);
 
   // initialize the transceiver on the SPI bus
   if (!radio.begin()) {
@@ -122,24 +125,6 @@ void loop() {
       Serial.println("Failure");
     }
 
-    // unsigned long start_timer = micros();                // start the timer
-    // bool report = radio.write(&payload, sizeof(float));  // transmit & save the report
-    // unsigned long end_timer = micros();                  // end the timer
-
-    // if (report) {
-    //   Serial.print(F("Transmission successful! "));  // payload was delivered
-    //   Serial.print(F("Time to transmit = "));
-    //   Serial.print(end_timer - start_timer);  // print the timer result
-    //   Serial.print(F(" us. Sent: "));
-    //   Serial.println(payload);  // print payload sent
-    //   payload += 0.01;          // increment float payload
-    // } else {
-    //   Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
-    // }
-
-    // // to make this example readable in the serial monitor
-    // delay(1000);  // slow transmissions down by 1 second
-
   } else {
     // This device is a RX node
 
@@ -153,9 +138,35 @@ void loop() {
       Serial.print(pipe);  // print the pipe number
       Serial.print(F(": "));
       Serial.println(payload);  // print the payload's value
+    
+
+    
+    if(payload > 0){
+      digitalWrite(direction_pin, HIGH);
+    }else{
+      digitalWrite(direction_pin, LOW);
+      payload *= -1;
     }
 
-    payload > 0 ? digitalWrite(direction_pin, HIGH) : digitalWrite(direction_pin, LOW);
+    digitalWrite(enable_pin, LOW);
+    delay(10);
+    int steps = (int)payload;
+    int i;
+
+    Serial.print("Stepper Enabled: ");
+    Serial.println(steps);
+
+    for(i=0; i<steps; i++)
+    {
+      digitalWrite(step_pin, HIGH);
+      delay(1);
+      digitalWrite(step_pin, LOW);
+      delay(10);
+    }
+
+    digitalWrite(enable_pin, HIGH);
+    Serial.println("Stepper Disabled");
+    }
     
   }  // role
 
